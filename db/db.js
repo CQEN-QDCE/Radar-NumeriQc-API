2eece10d044df9b70e6bb1d39573acbb01a990ce
jijp01 <pierre.jiji@sct.gouv.qc.ca>
2020-10-15 19:55:35 +0000

    modification de shema

--- db/db.js
+++ db/db.js
@@ -9,16 +9,42 @@ const pool = new Pool({
 });
 
 const query = `
+-- Schema radar_db --
+
+
+CREATE TABLE IF NOT EXISTS Axes (
+    id SERIAL NOT NULL,
+    nom VARCHAR(200) NOT NULL,
+    actif BOOLEAN,
+    PRIMARY KEY (id)
+);
+
+-- TODO Insérer les axes
+
+CREATE TABLE IF NOT EXISTS Pratiques (
+    id SERIAL NOT NULL,
+    id_axe INTEGER NOT NULL,
+    nom VARCHAR(200) NOT NULL,
+    actif BOOLEAN,
+    PRIMARY KEY (id),
+    FOREIGN KEY (id_axe) REFERENCES Axes(id) ON DELETE CASCADE
+);
+
+-- TODO Insérer les pratiques
+
 -- Table Enonces contient les enonces et leurs reponses --
 CREATE TABLE IF NOT EXISTS Enonces (
     id SERIAL NOT NULL,
+    id_pratique INTEGER NOT NULL,
     texte VARCHAR(1000) NOT NULL,
     reponse_0 VARCHAR(200) NOT NULL,
     reponse_1 VARCHAR(200) NOT NULL,
     reponse_2 VARCHAR(200) NOT NULL,
     reponse_3 VARCHAR(200) NOT NULL,
     reponse_4 VARCHAR(200) NOT NULL,
+    actif BOOLEAN,
     PRIMARY KEY (id),
+    FOREIGN KEY (id_pratique) REFERENCES Pratiques(id) ON DELETE CASCADE,
     UNIQUE (texte)
 );
 
@@ -26,14 +52,29 @@ CREATE TABLE IF NOT EXISTS Enonces (
 CREATE TABLE IF NOT EXISTS Organisations (
     id SERIAL NOT NULL,
     nom VARCHAR(200) NOT NULL,
+    actif BOOLEAN,
     PRIMARY KEY (id),
     UNIQUE (nom)
+);
+
+CREATE TABLE IF NOT EXISTS Organisateurs (
+    id SERIAL NOT NULL,
+    id_organisation INTEGER NOT NULL,
+    nom VARCHAR(200) NOT NULL,
+    prenom VARCHAR(200) NOT NULL,
+    courriel VARCHAR(200) NOT NULL,
+    actif BOOLEAN,
+    greenlight_userid VARCHAR(200), --À ajuster
+    PRIMARY KEY (id),
+    FOREIGN KEY (id_organisation) REFERENCES Organisations(id) ON DELETE CASCADE,
+    UNIQUE (courriel)
 );
 
 -- Table Seances contient les id des seances, id de l'organisation et si cette seance est officielle ou non (defaut False) --
 CREATE TABLE IF NOT EXISTS Seances (
     id INTEGER NOT NULL,
     id_organisation INTEGER NOT NULL,
+    participants TEXT[], --https://www.postgresql.org/docs/10/arrays.html
     est_officiel BOOLEAN NOT NULL DEFAULT FALSE,
     PRIMARY KEY (id),
     FOREIGN KEY (id_organisation) REFERENCES Organisations(id) ON DELETE CASCADE
@@ -44,8 +85,8 @@ CREATE INDEX SeancesIdOrganisation ON Seances(id_organisation);
 CREATE INDEX SeancesEstOfficiel ON Seances(est_officiel);
 
 -- Table Reponses contient les id des seances, les id des enonces et les reponses (entre 0 et 5) --
-CREATE TYPE type_reponse AS ENUM ('0', '1', '2', '3', '4', '5');
 
+CREATE TYPE type_reponse AS ENUM ('0', '1', '2', '3', '4', '5');
 CREATE TABLE IF NOT EXISTS Reponses (
     id SERIAL NOT NULL,
     id_seance INTEGER NOT NULL,
