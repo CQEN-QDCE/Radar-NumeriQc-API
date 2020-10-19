929599f2745ac6c23885b0b8f1e6fe2b7a294509
jijp01 <pierre.jiji@sct.gouv.qc.ca>
2020-10-19 15:20:44 +0000

    delete Cors

--- app.js
+++ app.js
@@ -6,18 +6,12 @@ const radarRouter = require('./routes/radar');
 const indexRouter = require('./routes/index');
 const testRouter = require('./routes/test');
 
-const cors = require('cors');
-
 // Configurer le middleware pour prendre en charge l'analyse des données JSON dans l'objet de requête
 app.use(express.json());
-
-// Configurer CORS
-app.use(cors());
 
 // configurer le router pour chaque chemin
 app.use(indexRouter);
 app.use('/api/', radarRouter);
-
 
 //  Créer un serveur pour écouter sur le port 5000
 app.listen(5000, () => {
