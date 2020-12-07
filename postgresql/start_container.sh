3b55a8b024dcea497253eff99a26a9d11dce0f0e
Viviane Orge <viviane.orge@sct.gouv.qc.ca>
2020-12-07 16:53:45 -0500

    fixer commentaires de code review

--- postgresql/start_container.sh
+++ postgresql/start_container.sh
@@ -6,11 +6,11 @@ if [ ! "$(docker ps -q -f name=radar_api_database)" ];
 then
     if [ "$(docker ps -aq -f status=exited -f name=radar_api_database)" ]; 
     then
-        docker start radar_db
+        docker start radar_api_database
     else
         docker run --name radar_api_database -e POSTGRES_USER=$POSTGRESQL_USER -e POSTGRES_PASSWORD=$POSTGRESQL_PASSWORD -e POSTGRES_DATABASE=$POSTGRESQL_DATABASE -d -p 5432:5432 radar_api_database
     fi
 else
-    docker stop radarAPI_database
-    docker start radarAPI_database
+    docker stop radar_api_database
+    docker start radar_api_database
 fi
